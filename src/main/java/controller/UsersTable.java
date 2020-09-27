ea32e648f950627160f19f92c4df6460d6db6096
Sebastian García Acosta <segaracos@outlook.com>
2020-09-27 09:40:16 -0500

    Users table done with custom new user form

--- src/main/java/controller/UsersTable.java
+++ src/main/java/controller/UsersTable.java
@@ -1,43 +1,43 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXComboBox;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
-public class UsersTable extends AnchorPane {
+public class UsersTable extends AnchorPane implements Initializable {
+	
+    @FXML
+    private JFXComboBox<String> sortByComboBox;
 	
 	public UsersTable() {
 		
 	}
 	
-    @FXML
-    void addNewRandomUser(ActionEvent event) {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		sortByComboBox.getItems().add("Id");
+		sortByComboBox.getItems().add("Name");
+		sortByComboBox.getItems().add("Date");
+		sortByComboBox.getItems().add("Cash");
 
+	}
+	
+    @FXML
+    void addNewUser(ActionEvent event) {
+    	
     }
 
-    @FXML
-    void openMainMenu(ActionEvent event) {
-
-    }
 
     @FXML
-    void tableSortByCash(ActionEvent event) {
-
-    }
-
-    @FXML
-    void tableSortById(ActionEvent event) {
-
-    }
-
-    @FXML
-    void tableSortByName(ActionEvent event) {
-
-    }
-
-    @FXML
-    void tableSortByTime(ActionEvent event) {
-
+    void sort(ActionEvent event) {
+    	String selecteCriteria = sortByComboBox.getValue();
+    	System.out.println(selecteCriteria);
     }
 
 }
